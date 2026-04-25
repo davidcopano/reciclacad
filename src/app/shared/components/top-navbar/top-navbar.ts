ce5b29c673602cfe8fe67ea6b1d72de764f1a9bc
David Copano <davidcopano96@gmail.com>
2026-04-25 15:01:19 +0200

    feat(app): added top-navbar component

---
+++ src/app/shared/components/top-navbar/top-navbar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-navbar',
+  imports: [],
+  templateUrl: './top-navbar.html',
+  styleUrl: './top-navbar.css',
+})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class TopNavbar {}
