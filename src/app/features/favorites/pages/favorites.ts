f5555473690f64ede5865f7f858d8adc578c934a
David Copano <davidcopano96@gmail.com>
2026-04-30 19:09:45 +0200

    feat(app): create favorites page

---
+++ src/app/features/favorites/pages/favorites.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites',
+  imports: [],
+  templateUrl: './favorites.html',
+  styleUrl: './favorites.css',
+})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export default class Favorites {}
