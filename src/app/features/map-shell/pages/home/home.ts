9297919f21d8eadb248aa207ed62a30a1c717094
David Copano <davidcopano96@gmail.com>
2026-04-30 19:07:14 +0200

    feat(app): create home page

---
+++ src/app/features/map-shell/pages/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterLink],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export default class Home {}
