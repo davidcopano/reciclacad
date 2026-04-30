6481d9b8bf7749ad127d2d24e34052912d1874cd
David Copano <davidcopano96@gmail.com>
2026-04-30 19:16:51 +0200

    feat(app): add favorites design and return to map button

--- src/app/features/favorites/pages/favorites.ts
+++ src/app/features/favorites/pages/favorites.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './favorites.html',
   styleUrl: './favorites.css',
 })
