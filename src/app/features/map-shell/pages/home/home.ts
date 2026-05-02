b856c04907111a84b65617bd674700d2dfbc7653
David Copano <davidcopano96@gmail.com>
2026-05-02 15:23:57 +0200

    feat(app): componetize side-navbar-searchbar

--- src/app/features/map-shell/pages/home/home.ts
+++ src/app/features/map-shell/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SideNavbarHeader } from '@shared/components/side-navbar/side-navbar-header/side-navbar-header';
+import { SideNavbarSearchbar } from '@shared/components/side-navbar/side-navbar-searchbar/side-navbar-searchbar';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, SideNavbarHeader],
+  imports: [RouterLink, SideNavbarHeader, SideNavbarSearchbar],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
---
+++ src/app/shared/components/side-navbar/side-navbar-searchbar/side-navbar-searchbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'side-navbar-searchbar',
+  imports: [],
+  templateUrl: './side-navbar-searchbar.html',
+})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class SideNavbarSearchbar {}
